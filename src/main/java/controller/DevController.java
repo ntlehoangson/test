705d25fcc7681f0a61cda2c69851d0bb7db0609c
hoangson <ntle.hoangson@gmail.com>
2024-03-29 17:43:26 +0700

    3

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -5,7 +5,7 @@ public class DevController {
         System.out.println(3);
     }
     public static void main(String[] args) {
-
+        System.out.println(23);
         System.out.println(1);
 
     }
