39dde386192d404a61673faa4425a418a8913c39
hoangson <ntle.hoangson@gmail.com>
2024-03-29 17:53:44 +0700

    dev2

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -7,6 +7,7 @@ public class DevController {
     public static void main(String[] args) {
         System.out.println(23);
         System.out.println("dev1");
+        System.out.println("dev2");
 
     }
     public static void s(String[] args) {
