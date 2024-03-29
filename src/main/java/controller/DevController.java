b0cd09432d68b79c3972b9bf6bb2183568faad90
hoangson <ntle.hoangson@gmail.com>
2024-03-29 13:52:28 +0700

    p1

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -3,5 +3,7 @@ package controller;
 public class DevController {
     public static void main(String[] args) {
         System.out.println("p1");
+        System.out.println("dev commit1");
     }
+
 }
