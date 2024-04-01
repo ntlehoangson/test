bf11385efc61d715d41980f552475d91ba99198f
hoangson <ntle.hoangson@gmail.com>
2024-04-01 11:39:26 +0700

    sugar

---
+++ src/main/java/com/example/test/hello/SugarController.java
@@ -0,0 +1,10 @@
+package com.example.test.hello;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SugarController {
+    public static void main(String[] args) {
+        System.out.println("sugar");
+    }
+}
