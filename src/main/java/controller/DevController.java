cd123a1198f72568602e611fce1ebad8562fcebf
hoangson <ntle.hoangson@gmail.com>
2024-04-01 11:39:54 +0700

    Revert "b3 3"
    This reverts commit fd03454d

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -9,7 +9,6 @@ public class DevController {
         System.out.println("dev1");
         System.out.println("dev2");
         System.out.println("dev3 3 ");
-        System.out.println("dev3 4 ");
 
     }
     public static void s(String[] args) {
