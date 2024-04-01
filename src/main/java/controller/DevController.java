0545e184765891b2d126b089571dc40bcee54ffb
hoangson <ntle.hoangson@gmail.com>
2024-04-01 10:22:58 +0700

    Revert "RESET"
    This reverts commit 7ddad24d877a4c32d62371b72d832220ea8340ce.

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -1,7 +1,28 @@
 package controller;
 
 public class DevController {
+    public static void ma1in(String[] args) {
+        System.out.println(3);
+    }
+    public static void main(String[] args) {
+        System.out.println(23);
+        System.out.println("dev1");
+        System.out.println("dev2");
+        System.out.println("dev3 3 ");
+        System.out.println("dev3 4 ");
+        System.out.println(4);
 
+    }
+    public static void s(String[] args) {
+        System.out.println("t la b3 1");
+        System.out.println("t la b3 2");
+
+
+    }
+    public static void s4(String[] args) {
+
+
+    }
 
 
 }
