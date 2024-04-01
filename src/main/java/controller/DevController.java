fae7031ad0cce08bd87121f788e2d3cb5bfcb0e1
hoangson <ntle.hoangson@gmail.com>
2024-04-01 10:58:20 +0700

    Revert "dev2"
    This reverts commit 39dde386192d404a61673faa4425a418a8913c39.

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -7,7 +7,6 @@ public class DevController {
     public static void main(String[] args) {
         System.out.println(23);
         System.out.println("dev1");
-        System.out.println("dev2");
 
     }
     public static void s(String[] args) {
