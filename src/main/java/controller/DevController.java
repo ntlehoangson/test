e695347b5ae3e2ac465810bd9dfe62944b8fec12
hoangson <ntle.hoangson@gmail.com>
2024-04-01 10:24:09 +0700

    Revert "new"
    This reverts commit 87f21418

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -6,16 +6,10 @@ public class DevController {
     }
     public static void main(String[] args) {
         System.out.println(23);
-        System.out.println("dev1");
-        System.out.println("dev2");
-        System.out.println("dev3 3 ");
-        System.out.println("dev3 4 ");
-        System.out.println(4);
+        System.out.println(3);
 
     }
     public static void s(String[] args) {
-        System.out.println("t la b3 1");
-        System.out.println("t la b3 2");
 
 
     }
