965ac64beaf483a9d856e53a9f28f8c9cf3b4e2c
hoangson <ntle.hoangson@gmail.com>
2024-04-16 17:25:33 +0700

    reset

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -1,10 +1,8 @@
 package controller;
 
 public class DevController {
-    public static void ma1in(String[] args) {
-        System.out.println("dev branch");
-        System.out.println("dev branch1");
-        System.out.println("dev branch2");
+    public static void main(String[] args) {
+
     }
 
 }
