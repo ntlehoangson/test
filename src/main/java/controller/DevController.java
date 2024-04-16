47377e031c4e6cda6ba30a1742ab008df1e7efc6
hoangson <ntle.hoangson@gmail.com>
2024-04-16 16:35:25 +0700

    dev

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -3,6 +3,8 @@ package controller;
 public class DevController {
     public static void ma1in(String[] args) {
         System.out.println("dev branch");
+        System.out.println("dev branch1");
+        System.out.println("dev branch2");
     }
 
 }
