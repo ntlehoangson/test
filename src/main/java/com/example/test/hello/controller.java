3f758e9f1c3983fc4f1b07458c3877b0bf66d8b0
hoangson <ntle.hoangson@gmail.com>
2024-04-16 16:18:57 +0700

    reset

--- src/main/java/com/example/test/hello/controller.java
+++ src/main/java/com/example/test/hello/controller.java
@@ -2,6 +2,5 @@ package com.example.test.hello;
 
 public class controller {
     public static void main(String[] args) {
-        System.out.println(2);
     }
 }
--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -2,23 +2,6 @@ package controller;
 
 public class DevController {
     public static void ma1in(String[] args) {
-        System.out.println(3);
     }
-    public static void main(String[] args) {
-        System.out.println(23);
-        System.out.println("dev1");
-        System.out.println("dev2");
-        System.out.println("dev3 3 ");
-
-    }
-    public static void s(String[] args) {
-
-
-    }
-    public static void s4(String[] args) {
-
-
-    }
-
 
 }
