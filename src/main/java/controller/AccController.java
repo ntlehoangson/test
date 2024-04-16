8bb51c7a32ec9c02f73c2d4da216b85954c14a58
hoangson <ntle.hoangson@gmail.com>
2024-04-16 16:24:53 +0700

    dev1

--- src/main/java/controller/AccController.java
+++ src/main/java/controller/AccController.java
@@ -4,4 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccController {
+    public static void main(String[] args) {
+        System.out.println("dev");
+    }
 }
