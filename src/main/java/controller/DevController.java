0c055ce333991e122ff205ef4924db6226a9505f
hoangson <ntle.hoangson@gmail.com>
2024-04-17 09:49:19 +0700

    dev1.1.1

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -2,7 +2,9 @@ package controller;
 
 public class DevController {
     public static void main(String[] args) {
-        System.out.println("dev2");
+        System.out.println("dev1");
+        System.out.println("dev1.1");
+        System.out.println("dev1.2");
     }
 
 }
