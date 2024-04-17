b5d0fc0ecfea1330aa55be84db66438b17f7b262
hoangson <ntle.hoangson@gmail.com>
2024-04-17 10:23:53 +0700

    Revert "reset"
    This reverts commit 965ac64b

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -2,9 +2,7 @@ package controller;
 
 public class DevController {
     public static void main(String[] args) {
-        System.out.println("dev1");
-        System.out.println("dev1.1");
-        System.out.println("dev1.2");
+
     }
 
 }
