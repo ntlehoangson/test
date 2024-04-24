86d6e8cec1942668916c17b6ad89f4b6a079b575
hoangson <ntle.hoangson@gmail.com>
2024-04-24 09:37:29 +0700

    Revert "2"
    This reverts commit 9dbaec67

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -2,8 +2,7 @@ package controller;
 
 public class DevController {
     public static void main(String[] args) {
-        System.out.println("3");
-        System.out.println("3");
+        System.out.println("2");
     }
 
 }
