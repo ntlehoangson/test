deb297204ba03c2d3c8d3305fbe2223156fe1f20
hoangson <ntle.hoangson@gmail.com>
2024-04-24 09:32:07 +0700

    3

--- src/main/java/controller/DevController.java
+++ src/main/java/controller/DevController.java
@@ -2,7 +2,8 @@ package controller;
 
 public class DevController {
     public static void main(String[] args) {
-        System.out.println("2");
+        System.out.println("3");
+        System.out.println("3");
     }
 
 }
