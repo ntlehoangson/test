9fded7da20a46b99bba2264cc89534faa845a275
hoangson <ntle.hoangson@gmail.com>
2024-05-08 16:04:36 +0700

    main1

--- src/main/java/controller/AccController.java
+++ src/main/java/controller/AccController.java
@@ -7,4 +7,7 @@ public class AccController {
     public static void main(String[] args) {
         System.out.println("d1");
     }
+    public static void main1(String[] args) {
+        System.out.println("main1");
+    }
 }
