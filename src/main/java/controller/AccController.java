8e38e020eaeab098458d7cdaf2fead5c6bf7bf03
hoangson <ntle.hoangson@gmail.com>
2024-05-08 15:51:40 +0700

    m3

--- src/main/java/controller/AccController.java
+++ src/main/java/controller/AccController.java
@@ -7,5 +7,6 @@ public class AccController {
     public static void main(String[] args) {
         System.out.println("d2");
         System.out.println("m2");
+        System.out.println("m3");
     }
 }
